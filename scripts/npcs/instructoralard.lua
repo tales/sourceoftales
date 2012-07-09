b5e5bc2c858a4e814edfa47c60d599e85a244de1
Jessica Tölke <jtoelke@mail.upb.de>
2012-07-09 17:30:39 +0200

    Adding tutorial logic and dialogue to instructor Alard.

--- scripts/npcs/instructoralard.lua
+++ scripts/npcs/instructoralard.lua
@@ -1,12 +1,47 @@
+-- variable use
+-- tutorial_fight: save progress on task to fight against the training dummies
+--      beat_dummies: got task
+--      done: finished that subquest
+-- tutorial_equip: saves if got equipment from smith
+
 local function instructorTalk(npc, ch)
     local function say(message)
         npc_message(npc, ch, message)
     end
+    local function fightTrainingSoldier(npc, ch)
+        say("Ah, there you are. Welcome to our unit. I'm going to teach you some basics.")
+        if tutorial_equip ~= "done" then
+            say("Didn't you get your equipment yet? Well, whatever, you can go to Smith Blacwin and get some armor after the training. Take this for now.")
+        else
+            say("Ah, I see you already got your armor. Here is your weapon.")
+        end
+        chr_inv_change(ch, "Shortsword", 1)
+        say ("Alright, now equip it and try it out on some of the training dummies.")
+        chr_set_quest(ch, "tutorial_fight", "beat_dummies")
+    end
 
-    say("TODO.")
-    -- tutorial: basic training
-    -- equipment, fighting
-    -- optional: explain attributes
+    local tutorial_fight = chr_get_quest(ch, "tutorial_fight")
+    local tutorial_equip = chr_get_quest(ch, "tutorial_equip")
+    if tutorial_fight == "beat_dummies" then
+        local dummies = chr_get_kill_count(ch, 1) -- TODO: remind Ablu to make this work with monster names
+        if dummies > 5 then -- TODO: define some constant or just leave it like this?
+            say("Alright, that looks good. Feel free to train here whenever you want.")
+            chr_set_quest(ch, "tutorial_fight", "done")
+        else
+            say("Come on, smash some more of the training dummies.")
+        end
+    else
+        if tutorial_fight == "done" then
+            if tutorial_equip ~= "done" then
+                say("You really should get your equipment now. Talk to Blacwin, the smith.")
+                say("He's a bit grumpy, so don't take it personal if he doesn't say much.")
+            end
+        else
+            fightTrainingSoldier(npc, ch)
+        end
+    end
+    say("Do you have any further questions?")
+    say("TODO: add some topics, e.g. attributes, skills etc")
 end
 
 local instructor = npc_create("Instructor Alard", 1, GENDER_MALE,
