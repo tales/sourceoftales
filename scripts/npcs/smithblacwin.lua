115e04435b9673858552480c420286d591e02cc3
Jessica Tölke <jtoelke@mail.upb.de>
2012-07-09 17:30:23 +0200

    Adding tutorial logic and dialogue to Smith Blacwin.

--- scripts/npcs/smithblacwin.lua
+++ scripts/npcs/smithblacwin.lua
@@ -1,9 +1,41 @@
+-- variable use
+-- tutorial_equip: saves if got equipment from smith
 local function smithTalk(npc, ch)
     local function say(message)
         npc_message(npc, ch, message)
     end
 
-    say("TODO.")
+    local function getEquipment(npc, ch)
+        say("Yes?")
+        local ask_equip = { "I need my equipment!",
+                            "Nevermind."}
+        local res = npc_choice(npc, ch, ask_equip)
+        if res == 1 then
+            say("Here.")
+            chr_inv_change(ch, "Kettle hat", 1)
+            chr_inv_change(ch, "Rusty chain armor", 1)
+            chr_set_quest(ch, "tutorial_equip", "done")
+            local reply_equip = { "You're not very talkative, are you?",
+                                "Thanks."}
+            local res = npc_choice(npc, ch, reply_equip)
+            if res == 1 then
+                say("No.")
+            else
+                say("Hm.")
+            end
+        else
+            say("Hmph.")
+        end
+    end
+
+    local tutorial_equip = chr_get_quest(ch, "tutorial_equip")
+    if tutorial_equip ~= "done" then
+        getEquipment(npc, ch)
+    else
+        say("TODO: tutorial_equip done")
+        -- maybe offer a shop with more equipment/weapons?
+        -- those stuff isn't intended to be sold, but he does that to boost his pay
+    end
 end
 
 local function smithWaypointReached(npc)
