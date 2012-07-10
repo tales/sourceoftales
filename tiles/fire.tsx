24dc53de73a25638bb3cdf017a901af60547eb64
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-07-10 18:49:20 +0200

    Added animated fire

---
+++ tiles/fire.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fire" tilewidth="32" tileheight="32">
+ <image source="fire.png" width="128" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="animation-delay0" value="15"/>
+   <property name="animation-delay1" value="15"/>
+   <property name="animation-delay2" value="15"/>
+   <property name="animation-frame0" value="1"/>
+   <property name="animation-frame1" value="2"/>
+   <property name="animation-frame2" value="3"/>
+  </properties>
+ </tile>
+</tileset>
