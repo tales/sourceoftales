cfee9b9a9d83a534da16ef3486ab9340f246a9f9
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-07-14 10:10:23 +0200

    Fixed string.starts() function

--- scripts/functions/stringutils.lua
+++ scripts/functions/stringutils.lua
@@ -4,5 +4,5 @@
 
 --- Returns wether a string starts with another one
 function string.starts(base_string, starting_string)
-   return string.sub(base_string, 1, string.len(base_string)) == starting_string
+   return string.sub(base_string, 1, string.len(starting_string)) == starting_string
 end
