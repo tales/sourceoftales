daa946df48b85581d6f9cdada65e6ba4ff89b68d
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-07-15 12:20:53 +0200

    Moved spawn to village map script wise

---
+++ scripts/maps/village.lua
@@ -0,0 +1,24 @@
+--[[
+    The starting map.
+--]]
+
+atinit(function()
+    require "scripts/functions/npchelper"
+    require "scripts/functions/triggerhelper"
+
+    require "scripts/npcs/village/guardfordwin"
+    require "scripts/npcs/village/guardterric"
+    require "scripts/npcs/village/instructoralard"
+    require "scripts/npcs/village/recruithugh"
+    require "scripts/npcs/village/recruitmaurice"
+    require "scripts/npcs/village/smithblacwin"
+    require "scripts/npcs/village/veterangodwin"
+
+    require "scripts/npcs/village/emma"
+    require "scripts/npcs/village/merchantanabel"
+    require "scripts/npcs/village/merchantgilbert"
+    require "scripts/npcs/village/merchantwalter"
+    require "scripts/npcs/village/monkmartin"
+
+end)
+
