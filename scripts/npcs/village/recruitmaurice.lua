7bb71845690a993adbf952001741b7d90d6a071f
Jessica Tölke <jtoelke@mail.upb.de>
2012-07-16 19:53:53 +0200

    Adding sympathy logic to Recruit Maurice.

--- scripts/npcs/village/recruitmaurice.lua
+++ scripts/npcs/village/recruitmaurice.lua
@@ -5,40 +5,59 @@ local function recruitTalk(npc, ch)
         npc_message(npc, ch, message)
     end
 
-    say("Hello " .. being_get_name(ch) .. ". Let's talk a bit.")
+    local function chat()
+        say("Hello " .. being_get_name(ch) .. ". Let's talk a bit.")
 
-    local choices = { "Why did you become a soldier?",
-                    "Do you know why they're hiring so many recruits?",
-                    "I need to go." }
-
-    local res = npc_choice(npc, ch, choices)
-
-    if res == 1 then
-        say("Oh, I just don't want to spent the rest of my life with farm work. You know, I've got so many brothers "..
-            "and sisters, I didn't like the thought to drudge on a dirty farm all my life for my oldest brothers "..
-            "benefit. So I ran away when I was twelve.")
-        say("I tramped around the area some while, finding some work to do here and there, but that's a cumbersome "..
-            "life. When I met the recruiter, I decided to rather become I soldier. I'm going to see the world, and "..
-            "maybe I can find some nice place to live after my time of service.")
-    elseif res == 2 then
-        say("Because of war, you dumbhead. Why else should they even come out here to Goldenfield to hire people? "..
-            "I mean, nothing ever happens here and usually there are only a handful of old soldiers here in the "..
-            "casern who feel bored to death.")
-        say("After the basic training we're probably going to be send to more interesting places.")
-        say("At least I hope so. If those rebels become a more serious problem, they might need use here.")
-
-        local choices = { "Well, thanks for the information.",
-                        "Rebels? What do you mean?"}
+        local choices = { "Why did you become a soldier?",
+                        "Do you know why they're hiring so many recruits?",
+                        "I need to go." }
 
         local res = npc_choice(npc, ch, choices)
 
-        if res == 2 then
-            say("Didn't you hear? Some people got upset about the increased taxes and started rioting. So far they "..
-                "didn't seem very organized, but I heard rumors that they're getting more successful.")
+        if res == 1 then
+            say("Oh, I just don't want to spent the rest of my life with farm work. You know, I've got so many brothers "..
+                "and sisters, I didn't like the thought to drudge on a dirty farm all my life for my oldest brothers "..
+                "benefit. So I ran away when I was twelve.")
+            say("I tramped around the area some while, finding some work to do here and there, but that's a cumbersome "..
+                "life. When I met the recruiter, I decided to rather become I soldier. I'm going to see the world, and "..
+                "maybe I can find some nice place to live after my time of service.")
+        elseif res == 2 then
+            say("Because of war, you dumbhead. Why else should they even come out here to Goldenfield to hire people? "..
+                "I mean, nothing ever happens here and usually there are only a handful of old soldiers here in the "..
+                "casern who feel bored to death.")
+            say("After the basic training we're probably going to be send to more interesting places.")
+            say("At least I hope so. If those rebels become a more serious problem, they might need use here.")
+
+            local choices = { "Well, thanks for the information.",
+                            "Rebels? What do you mean?"}
+
+            local res = npc_choice(npc, ch, choices)
+
+            if res == 2 then
+                say("Didn't you hear? Some people got upset about the increased taxes and started rioting. So far they "..
+                    "didn't seem very organized, but I heard rumors that they're getting more successful.")
+            end
+        elseif res == 3 then
+            say("Oh, ok. See you later then.")
         end
-    elseif res == 3 then
-        say("Oh, ok. See you later then.")
     end
+
+    local sympathy = tonumber(chr_get_quest(ch, "soldier_sympathy"))
+    if (sympathy == nil) then
+        sympathy = 0
+    end
+
+    if sympathy >= SYMPATHY_NEUTRAL then
+        chat()
+    elseif sympathy > SYMPATHY_RELUCTANT then
+        say("To get amnesty for your misconducts talk to TODO.")
+        sympathy = sympathy - 1
+        chr_set_quest(ch, "soldier_sympathy", tostring(sympathy))
+    else -- sympathy <= SYMPATHY_RELUCTANT
+        say("Traitor!")
+        being_damage(ch, 80, 10, 9999, DAMAGE_PHYSICAL, ELEMENT_NEUTRAL)
+    end
+
 end
 
 local recruit = create_npc_by_name("Recruit Maurice", recruitTalk)
