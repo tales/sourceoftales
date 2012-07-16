a97010a9d3d699786741c8c16b5788bf04c3fe86
Jessica Tölke <jtoelke@mail.upb.de>
2012-07-16 19:54:10 +0200

    Adding sympathy logic to Smith Blacwin.

--- scripts/npcs/village/smithblacwin.lua
+++ scripts/npcs/village/smithblacwin.lua
@@ -13,46 +13,60 @@ local function smithTalk(npc, ch)
         npc_message(npc, ch, message)
     end
 
-    local function getEquipment()
-        say("Yes?")
-        local ask_equip = { "I need my equipment!",
-                            "Nevermind."}
-        local res = npc_choice(npc, ch, ask_equip)
-        if res == 1 then
-            say("Here.")
-            chr_inv_change(ch, "Kettle hat", 1)
-            chr_inv_change(ch, "Rusty chain armor", 1)
-            chr_set_quest(ch, "tutorial_equip", "done")
-            local reply_equip = { "You're not very talkative, are you?",
-                                "Thanks."}
-            local res = npc_choice(npc, ch, reply_equip)
-            if res == 1 then
-                say("No.")
-            else
-                say("Hm.")
-            end
-        else
-            say("Hmph.")
-        end
-    end
-
-    local tutorial_equip = chr_get_quest(ch, "tutorial_equip")
-    local tutorial_shop = chr_get_quest(ch, "tutorial_shop")
-    if tutorial_equip ~= "done" then
-        getEquipment()
-    else
-        say("Hrm.")
-        if tutorial_shop == "done" then
-            local ask_shop = { "I heard you could sell me better armor?",
+    local function chat()
+        local tutorial_equip = chr_get_quest(ch, "tutorial_equip")
+        local tutorial_shop = chr_get_quest(ch, "tutorial_shop")
+        if tutorial_equip ~= "done" then
+            say("Yes?")
+            local ask_equip = { "I need my equipment!",
                                 "Nevermind."}
-            local res = npc_choice(npc, ch, ask_shop)
+            local res = npc_choice(npc, ch, ask_equip)
             if res == 1 then
-                say("TODO: shop")
-                -- those stuff isn't intended to be sold, but he does that to boost his pay
+                say("Here.")
+                chr_inv_change(ch, "Kettle hat", 1)
+                chr_inv_change(ch, "Rusty chain armor", 1)
+                chr_set_quest(ch, "tutorial_equip", "done")
+                local reply_equip = { "You're not very talkative, are you?",
+                                    "Thanks."}
+                local res = npc_choice(npc, ch, reply_equip)
+                if res == 1 then
+                    say("No.")
+                else
+                    say("Hm.")
+                end
             else
                 say("Hmph.")
             end
+        else
+            say("Hrm.")
+            if tutorial_shop == "done" then
+                local ask_shop = { "I heard you could sell me better armor?",
+                                    "Nevermind."}
+                local res = npc_choice(npc, ch, ask_shop)
+                if res == 1 then
+                    say("TODO: shop")
+                    -- those stuff isn't intended to be sold, but he does that to boost his pay
+                else
+                    say("Hmph.")
+                end
+            end
         end
+    end
+
+    local sympathy = tonumber(chr_get_quest(ch, "soldier_sympathy"))
+    if (sympathy == nil) then
+        sympathy = 0
+    end
+
+    if sympathy >= SYMPATHY_NEUTRAL then
+        chat()
+    elseif sympathy > SYMPATHY_RELUCTANT then
+        say("Go away until you've talked to TODO.")
+        sympathy = sympathy - 1
+        chr_set_quest(ch, "soldier_sympathy", tostring(sympathy))
+    else -- sympathy <= SYMPATHY_RELUCTANT
+        say("Argh!")
+        being_damage(ch, 120, 10, 9999, DAMAGE_PHYSICAL, ELEMENT_NEUTRAL)
     end
 end
 
