3571348c0724a69c917c3f2fe9ac1125f7bc84c6
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-07-17 22:09:35 +0200

    Updated guard patrol to stay close to waypoints

--- scripts/functions/guardpatrol.lua
+++ scripts/functions/guardpatrol.lua
@@ -39,16 +39,28 @@ function GuardPatrol:logic()
         end
     end
 
-    -- No enemys, handle walkcycle
-    if #enemys == 0 then
-        Patrol.logic(self)
-        return
-    end
+    local enemy_count = 0
 
     for _, member in ipairs(self.members) do
+        -- Check for out of range enemys
+        local anger_list = monster_get_angerlist(member)
+        enemy_count = enemy_count + #anger_list
+        for ch, anger in pairs(anger_list) do
+            if get_distance(posX(ch), posY(ch), x, y) > self.track_range * 2 then
+                monster_drop_anger(member, ch)
+                enemy_count = enemy_count - 1
+            end
+        end
+
         for _, enemy in ipairs(enemys) do
             monster_change_anger(member, enemy, 1)
         end
+    end
+
+    -- No enemys, handle walkcycle
+    if enemy_count == 0 then
+        Patrol.logic(self)
+        return
     end
 end
 
