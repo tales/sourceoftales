0233b01987c7b5407b9f28027c750bee1ab94de5
Jessica Tölke <jtoelke@mail.upb.de>
2012-07-24 11:51:21 +0200

    Fix error in script of Recruit Jordan: add missing '..'

--- scripts/npcs/casern_north/recruitjordan.lua
+++ scripts/npcs/casern_north/recruitjordan.lua
@@ -4,7 +4,7 @@ local function recruitTalk(npc, ch)
     local function say(message)
         npc_message(npc, ch, message)
     end
-    say("This is nice. Everything is calm and peaceful. "
+    say("This is nice. Everything is calm and peaceful. "..
         "I mean, we're getting paid for sitting around here, drinking and eating. Isn't that wonderful?")
 end
 
