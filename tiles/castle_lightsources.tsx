a0941a3d57ce5c8ebe95a130e7930f4f039e34cb
Stefan Beller <stefanbeller@googlemail.com>
2012-07-30 00:54:28 +0200

    Export lightsources to tsx.

---
+++ tiles/castle_lightsources.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="castle_lightsources" tilewidth="32" tileheight="96">
+ <image source="castle_lightsources.png" width="160" height="320"/>
+  <tile id="0">
+   <properties>
+    <property name="animation-delay0" value="20"/>
+    <property name="animation-delay1" value="40"/>
+    <property name="animation-delay2" value="20"/>
+    <property name="animation-frame0" value="0"/>
+    <property name="animation-frame1" value="5"/>
+    <property name="animation-frame2" value="10"/>
+   </properties>
+  </tile>
+  <tile id="1">
+   <properties>
+    <property name="animation-delay0" value="30"/>
+    <property name="animation-delay1" value="30"/>
+    <property name="animation-delay2" value="30"/>
+    <property name="animation-frame0" value="1"/>
+    <property name="animation-frame1" value="6"/>
+    <property name="animation-frame2" value="11"/>
+   </properties>
+  </tile>
+  <tile id="2">
+   <properties>
+    <property name="animation-delay0" value="30"/>
+    <property name="animation-delay1" value="30"/>
+    <property name="animation-delay2" value="20"/>
+    <property name="animation-frame0" value="2"/>
+    <property name="animation-frame1" value="7"/>
+    <property name="animation-frame2" value="12"/>
+  </properties>
+  </tile>
+ <tile id="3">
+  <properties>
+   <property name="animation-delay0" value="30"/>
+   <property name="animation-delay1" value="30"/>
+   <property name="animation-delay2" value="30"/>
+   <property name="animation-frame0" value="3"/>
+   <property name="animation-frame1" value="8"/>
+   <property name="animation-frame2" value="13"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="animation-delay0" value="20"/>
+   <property name="animation-delay1" value="40"/>
+   <property name="animation-delay2" value="20"/>
+   <property name="animation-frame0" value="4"/>
+   <property name="animation-frame1" value="9"/>
+   <property name="animation-frame2" value="14"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ tiles/castle_lightsources_small.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="castle_lightsources_small" tilewidth="32" tileheight="32">
+ <image source="castle_lightsources.png" width="160" height="320"/>
+</tileset>
