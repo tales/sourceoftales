60754e69cfb37e99e311837bc2406ca7fbe4178b
Stefan Beller <stefanbeller@googlemail.com>
2012-07-30 14:05:39 +0200

    small castle_lightsources also animated.

--- tiles/castle_lightsources_small.tsx
+++ tiles/castle_lightsources_small.tsx
@@ -2,4 +2,64 @@
 <!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
 <tileset name="castle_lightsources_small" tilewidth="32" tileheight="32">
  <image source="castle_lightsources.png" width="160" height="320"/>
+  <tile id="0">
+   <properties>
+    <property name="animation-delay0" value="20"/>
+    <property name="animation-delay1" value="40"/>
+    <property name="animation-delay2" value="20"/>
+    <property name="animation-frame0" value="0"/>
+    <property name="animation-frame1" value="15"/>
+    <property name="animation-frame2" value="30"/>
+   </properties>
+  </tile>
+  <tile id="1">
+   <properties>
+    <property name="animation-delay0" value="30"/>
+    <property name="animation-delay1" value="30"/>
+    <property name="animation-delay2" value="30"/>
+    <property name="animation-frame0" value="1"/>
+    <property name="animation-frame1" value="16"/>
+    <property name="animation-frame2" value="31"/>
+   </properties>
+  </tile>
+  <tile id="2">
+   <properties>
+    <property name="animation-delay0" value="30"/>
+    <property name="animation-delay1" value="30"/>
+    <property name="animation-delay2" value="20"/>
+    <property name="animation-frame0" value="2"/>
+    <property name="animation-frame1" value="17"/>
+    <property name="animation-frame2" value="32"/>
+  </properties>
+  </tile>
+ <tile id="3">
+  <properties>
+   <property name="animation-delay0" value="30"/>
+   <property name="animation-delay1" value="30"/>
+   <property name="animation-delay2" value="30"/>
+   <property name="animation-frame0" value="3"/>
+   <property name="animation-frame1" value="18"/>
+   <property name="animation-frame2" value="33"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="animation-delay0" value="20"/>
+   <property name="animation-delay1" value="40"/>
+   <property name="animation-delay2" value="20"/>
+   <property name="animation-frame0" value="4"/>
+   <property name="animation-frame1" value="19"/>
+   <property name="animation-frame2" value="34"/>
+  </properties>
+ </tile>
+ <tile id="5">
+   <properties>
+    <property name="animation-delay0" value="10"/>
+    <property name="animation-delay1" value="30"/>
+    <property name="animation-delay2" value="30"/>
+    <property name="animation-frame0" value="5"/>
+    <property name="animation-frame1" value="20"/>
+    <property name="animation-frame2" value="35"/>
+   </properties>
+  </tile>
 </tileset>
