c1aa155a0f32e918c620574939ec811a020c7af3
Erik Schilling <ablu.erikschilling@googlemail.com>
2012-08-01 12:39:48 +0200

    Added missing copyright note

--- scripts/items/gold_less.lua
+++ scripts/items/gold_less.lua
@@ -1,3 +1,24 @@
+--[[
+
+  Pile of gold that turns into real money on pickup
+
+  Copyright (C) 2012 Erik Schilling
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--]]
+
 local money = get_item_class("Few Gold Coins")
 
 local amount_min = 3
