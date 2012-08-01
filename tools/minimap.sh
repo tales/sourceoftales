917a376a4a6ae4fcd4d8501ec9db12363d38bea2
Przemysław Grzywacz <nexather@gmail.com>
2012-08-01 23:12:25 +0200

    minimaps blended original + sepia

--- tools/minimap.sh
+++ tools/minimap.sh
@@ -1,1 +1,3 @@
-convert $1 -sepia-tone 75% $2
+#convert $1 -sepia-tone 75% $2
+#convert $1 \( +clone -sepia-tone 30% \) -average  $2
+convert $1 \( +clone -sepia-tone 90% \) -average -darken 10% -alpha off $2
