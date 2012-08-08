ed0b00fb5f8eb959a6e2a953a5ec5055bf21d297
Socapex <philippe_groarke@yahoo.ca>
2012-08-08 17:31:58 -0400

    casern_storage script

---
+++ scripts/maps/casern_storage.lua
@@ -0,0 +1,74 @@
+--[[
+
+  Inside Casern Storage (Smith Blackwin's workshop)
+
+  Copyright (C) 2012 Philippe Groarke
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--]]
+
+
+local soldierpatrol = SoldierPatrol:new("SoldierPatrol", 4 * TILESIZE, REPUTATION_RELUCTANT)
+schedule_every(1, function() soldierpatrol:logic() end)
+
+local soldierspawned = false
+
+local function respawn(patrol, mob, amount)
+    local x = patrol.path[patrol.position_index].x
+    local y = patrol.path[patrol.position_index].y
+    for i=1, amount do
+        patrol:assignBeing(monster_create(mob, x, y))
+    end
+end
+
+local function rebelphilip_daggers(being, id)
+
+    if being_type(being) ~= TYPE_CHARACTER then
+        return
+    end
+
+    local quest = chr_try_get_quest(being, "rebelphilip_daggers")
+
+    if quest == "started" then
+
+        if soldierspawned == false then
+
+            if #soldierpatrol.members == 0 then
+                respawn(soldierpatrol, "Soldier", 1)
+
+                soldierspawned = true
+                --item_drop(160, 288, 61)
+
+            end
+        end
+    end
+end
+
+
+
+
+atinit(function()
+    --require "scripts/items"
+    require "scripts/functions/triggerhelper"
+    require "scripts/functions/guardpatrol"
+    require "scripts/functions/soldierpatrol"
+
+    parse_triggers_from_map()
+
+    create_trigger_by_name("rebelphilip daggers quest", rebelphilip_daggers)
+
+    --item_drop(160, 288, 61)
+
+    end)
