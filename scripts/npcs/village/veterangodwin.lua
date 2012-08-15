82c77c0b1fe961cd7ce3eb111668118e4ab1eccc
Jessica Tölke <jtoelke@mail.upb.de>
2012-08-15 17:45:36 +0200

    Clean-up of Veteran Godwin's script.
    Adapt to latest version of scripting guidelines.
    Remove call of bounty function, send to Lieutenant Bennet instead.

--- scripts/npcs/village/veterangodwin.lua
+++ scripts/npcs/village/veterangodwin.lua
@@ -28,47 +28,60 @@ local function veteran_talk(npc, ch)
     local function say(message)
         npc_message(npc, ch, message)
     end
+
     local function send_tutorial()
-        say("Hey, rookie. You aren't paid for standing in the landscape and looking like a sheep.")
-        say("You should better hurry to get to the basic training unless you want to do extra hours in the kitchen "..
-            "during the next month.")
+        say("Hey, rookie. You aren't paid for standing in the landscape "
+            .. "and looking like a sheep.")
+        say("You should better hurry to get to the basic training unless you "
+            .. "want to do extra hours in the kitchen during the next month.")
 
-        local choices = { "In the kitchen? I'm a strong fighter!",
-                        "Alright, thank you, where do I have to go?",
-                        "I was promised fame and gold for joining the army!" }
-
+        local choices = {
+            "In the kitchen? I'm a strong fighter!",
+            "Alright, thank you, where do I have to go?",
+            "I was promised fame and gold for joining the army!"
+        }
         local res = npc_choice(npc, ch, choices)
 
         if res == 1 then
-            say("Don't make me laugh! I'd be surprised if you'd know at what end to hold a sword.")
+            say("Don't make me laugh! I'd be surprised if you'd know at what "
+                .. "end to hold a sword.")
         elseif res == 2 then
             say("Watch out, this isn't a friendly place for a wimp.")
         elseif res ==3 then
-            say("Hah, recruiters are liers. It's their job to tell fairytales about fame to dumbheads like you.")
-            say("The only thing that's awaiting you is a lot of hard work, boy.")
+            say("Hah, recruiters are liers. It's their job to tell fairytales "
+                .. "about fame to dumbheads like you.")
+            say("The only thing that's awaiting you is a lot of hard work, "
+                .. "boy.")
         end
 
-        say("Now go, talk to Instructor Alard, so he can show you how you can avoid being speared by the first "..
-            "enemy you'll encounter.")
-        say("Oh, and get your equipment from Blacwin. You should at least look like a soldier.")
+        say("Now go, talk to Instructor Alard, so he can show you how you can "
+            .. "avoid being speared by the first enemy you'll encounter.")
+        say("Oh, and get your equipment from Blacwin. "
+            .. "You should at least look like a soldier.")
         say ("Come back to me when you're done.")
     end
 
     local function collect_taxes()
         local taxes = chr_get_quest(ch, "soldier_goldenfields_taxes")
         if (taxes == "") then
-            say("You're done with your basic training? Very well. I've got some task for you. "..
-                "The Innkeeper from Goldenfields, Norman, is late with paying his taxes. "..
-                "There was quite some moaning among the villagers because of the extra taxes due to the war. "..
-                "Hah! We're the ones keeping them safe, ungrateful wretches.")
-            say("I want you to get the outstanding ".. GOLDENFIELDS_TAXES .. " GP from that innkeeper.")
+            say("You're done with your basic training? Very well. I've got "
+                .. "some task for you. The Innkeeper from Goldenfields, "
+                .. "Norman, is late with paying his taxes. There was quite "
+                .. "some moaning among the villagers because of the extra "
+                .. "taxes due to the war. Hah! We're the ones keeping "
+                .. "them safe, ungrateful wretches.")
+            say("I want you to get the outstanding " .. GOLDENFIELDS_TAXES
+                .. " GP from that innkeeper.")
             chr_set_quest(ch, "soldier_goldenfields_taxes", "gotorder")
-            say("You can find the Inn in Goldenfields south east of the casern.")
+            say("You can find the Inn in Goldenfields south east of the "
+                .. "casern.")
         else
             say("Did you get the money from that innkeeper?")
-            local choices = { "I'm on my way.",
-                            "Here it is.",
-                            "I can't find it, where do I have to go." }
+            local choices = {
+                "I'm on my way.",
+                "Here it is.",
+                "I can't find it, where do I have to go?"
+            }
             local res = npc_choice(npc, ch, choices)
             if res == 2 then
                 local money = chr_money(ch)
@@ -79,16 +92,20 @@ local function veteran_talk(npc, ch)
                     chr_money_change(ch, 40)
                     say("Well done, kid.")
                 else
-                    say("Where is the money? Did you spend it on booze? Kid, this isn't a place to fool around. "..
-                        "Get me the ".. GOLDENFIELDS_TAXES .. " GP.")
+                    say("Where is the money? Did you spend it on booze? Kid, "
+                        .. "this isn't a place to fool around. "
+                        .. "Get me the ".. GOLDENFIELDS_TAXES .. " GP.")
                 end
             elseif res == 3 then
                 say("You're not very clever, are you? Alright, listen.")
-                say("When you leave the casern, go south east over the Goldenfields market. Don't let those "..
-                    "brash merchants distract you. Further into the village there are also some food stands.")
-                say("If you follow the way to the east, you'll get to some small bridge over a stream. Cross it and "..
-                    "follow the stream to the west again and you'll find the entrance to what they call a pub in this "..
-                    "place.")
+                say("When you leave the casern, go south east over the "
+                    .. "Goldenfields market. Don't let those brash merchants "
+                    .. "distract you. Further into the village there are also "
+                    .. "some food stands.")
+                say("If you follow the way to the east, you'll get to some "
+                    .. "small bridge over a stream. Cross it and follow the "
+                    .. "stream to the west again and you'll find the entrance "
+                    .. "to what they call a pub in this place.")
             end
         end
     end
@@ -106,14 +123,16 @@ local function veteran_talk(npc, ch)
         elseif (taxes ~= "done") then
             collect_taxes()
         else
-        say("I don't have anything specific for you to do right now. "..
-            "But you can help by chasing some of the rebels in the forest. And better not go alone, you still "..
-            "look a bit too weakly to deal with them on your own.")
-        say("We give out rewards for many defeated rebels.")
-            goldenfields_check_bounty(npc, ch, "soldier_goldenfields_killrebels", "Rebel")
+            say("I don't have anything specific for you to do right now. "
+                .. "But you can help by chasing some of the rebels in the "
+                .. "forest. And better not go alone, you still look a bit "
+                .. "look a bit too weakly to deal with them on your own.")
+            say("We give out rewards for many defeated rebels. Lieutenant "
+                .. "Bennet on the second floor of the casern gives them out.")
         end
     elseif reputation > REPUTATION_RELUCTANT then
-        say("Why are you here? Talk to Magistrate Eustace to get amnesty from your crimes!")
+        say("Why are you here? "
+            .. "Talk to Magistrate Eustace to get amnesty from your crimes!")
         change_reputation(ch, "soldier_reputation", "Army", -1)
     else -- reputation <= REPUTATION_RELUCTANT
         say("I can't deny you're brave, but that won't help you now!")
@@ -121,7 +140,6 @@ local function veteran_talk(npc, ch)
     end
     patrol:unblock(ch)
 end
--- TODO: add start equipment in global_events.lua, on_chr_birth
 
 local veteran = create_npc_by_name("Veteran Godwin", veteran_talk)
 
