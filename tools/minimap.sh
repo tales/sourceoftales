998cd78ef2915fc3f29bd568cebcb0a61d535b19
Przemysław Grzywacz <nexather@gmail.com>
2013-04-20 20:26:40 +0200

    Moved maps to subdirectories.

--- tools/minimap.sh
+++ tools/minimap.sh
@@ -1,3 +1,4 @@
 #convert $1 -sepia-tone 75% $2
 #convert $1 \( +clone -sepia-tone 30% \) -average  $2
-convert $1 \( +clone -sepia-tone 90% \) -average -darken 10% -alpha off $2
+#convert $1 \( +clone -sepia-tone 90% \) -average -darken 10% -alpha off $2
+convert $1 \( +clone -sepia-tone 90% \) -average -brightness-contrast -5x2 -alpha off $2
