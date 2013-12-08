f1d852ebb4e4ea5dbd3ae3b06913de527676fae8
Erik Schilling <ablu.erikschilling@gmail.com>
2013-12-08 22:47:23 +0100

    Removed level from LEVELUP chatmessage

--- scripts/attributes.lua
+++ scripts/attributes.lua
@@ -111,8 +111,8 @@ function Entity:give_experience(experience, attribute)
     local old_experience = self:base_attribute(attribute or "XP")
     local old_level = self:level()
     self:set_base_attribute(attribute or "XP", old_experience + experience)
-    if self:level() > old_level then
-        self:say("LEVELUP!!! " .. self:level())
+    f self:level() > old_level then
+        self:say("LEVELUP!!!")
         self:set_attribute_points(self:attribute_points() + 1)
         self:set_correction_points(self:correction_points() + 1)
     end
